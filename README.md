8995a509371f27f17fbc1f52cb0cc215c9bc3bf4
Suriyadeepan Ram <suriyadeepan@acm.org>
2016-04-19 11:25:07 +0530

    add SynBot

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dumbots
 A list of dumb bots for experimentation, hooked via telegram.
+
+## List of Bots
+
+1. SynBot : meaning, synonyms, antonyms of words
---
+++ bots/SynBot/SynBot.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Simple Bot to reply to Telegram messages
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+This Bot uses the Updater class to handle the bot.
+
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+from telegram.ext import Updater
+import logging
+import os
+from nltk.corpus import wordnet as wn
+
+
+# Enable logging
+logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+def start(bot, update):
+    bot.sendMessage(update.message.chat_id, text='Hi!')
+
+
+def help(bot, update):
+    bot.sendMessage(update.message.chat_id, text='Help!')
+
+
+def echo(bot, update):
+    bot.sendMessage(update.message.chat_id, text=update.message.text)
+
+def reply(bot, update):
+    # get word
+    word = update.message.text
+    ERROR_STR = 'Only a meaningful WORD is accepted!'
+    if ' ' not in word:
+        print 'Message : ',word
+        synms = wn.synsets(word)
+        if synms:
+            reply_msg = synms[0].definition()
+            print 'Reply : ',reply
+            bot.sendMessage(update.message.chat_id, text=reply_msg)
+        else:
+            bot.sendMessage(update.message.chat_id, text=ERROR_STR)
+    else:
+        bot.sendMessage(update.message.chat_id, text=ERROR_STR)
+
+
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+
+def main():
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater(os.environ['SYN_BOT_TOKEN'])
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.addTelegramCommandHandler("start", start)
+    dp.addTelegramCommandHandler("help", help)
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.addTelegramMessageHandler(reply)
+
+    # log all errors
+    dp.addErrorHandler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    print 'Bot Started!!\n'
+
+    # Run the bot until the you presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
