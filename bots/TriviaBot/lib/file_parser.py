1ab3d107b33c2cd2b01428f4a4b674dd4d89d382
naveenprasanth <nnpths01@gmail.com>
2016-04-22 15:44:10 +0530

    minor changes

--- bots/TriviaBot/lib/file_parser.py
+++ bots/TriviaBot/lib/file_parser.py
@@ -1,42 +1,52 @@
 import numpy as np
 import hickle as hkl
+from os import walk
 
-def file2hickle ( category ) :
+
+def file2list ( category ) :
     prefix = "data/"
-    category = prefix + category 
+    category = prefix + category
     file = open( category )
-    listofcontents = [] 
+    listofcontents = []
     choicelist = []
     contents ={}
     linenumber=0
-    
+
     for line in file:
         if line.startswith('#'):
             contents['ques'] = line[2:]
+            linenumber += 1
         elif line.startswith('^'):
             contents['ans']= line[2:]
+            linenumber += 1
         elif line[0] in ['A','B','C','D']:
             choicelist.append(line[2:])
+            linenumber += 1
             if line.startswith('D'):
                 choicelist.sort()
                 contents['ansnum'] = choicelist.index(contents['ans'])
                 choicestring = ""
                 for i in range(len(choicelist)):
-                    choicestring += "[" + str(i) + "] " + choicelist[i] + " "  
-                
+                    choicestring += "[" + str(i) + "] " + choicelist[i] + " "
+
                 contents["choices"] = choicestring
                 listofcontents.append(contents)
-                #print "question : ",contents["ques"],"answer :",contents['ansnum'],contents["ans"],"choice :",contents["choices"]
-                choicelist = []  
+                #print "Line :",linenumber,"question : ",contents["ques"],"answer :",contents['ansnum'],contents["ans"],"choice :",contents["choices"]
+                choicelist = []
                 contents = {}
     return listofcontents
 
 # pick a category
-l = file2hickle("people")
-print 'Len("people") : ', len(l)
+mypath = "../data"
+files = []
+for (dirpath, dirnames, filenames) in walk(mypath):
+    files.extend(filenames)
+    break
 
-# hickle name
-hkl_name = 'people.hkl'
-
-# save as hickle
-hkl.dump(l,hkl_name)
+for file in files:
+    print "converting the category",file
+    l = file2list(file)
+    # hickle name
+    hkl_name = "../data/hkldata/"+file+'.hkl'
+    # save as hickle
+    hkl.dump(l,hkl_name)
